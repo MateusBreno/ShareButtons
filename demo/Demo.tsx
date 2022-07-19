62398ea5013f686fff217edb0b7eb76baf8ebb2c
MateusBreno <brenoconstantino80@gmail.com>
2022-07-19 19:42:28 -0300

    first commit

---
+++ demo/Demo.tsx
@@ -0,0 +1,141 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable import/no-unresolved, import/extensions, import/no-extraneous-dependencies */
+import React, { Component } from 'react';
+import {
+  FacebookShareCount,
+  PinterestShareCount,
+  VKShareCount,
+  OKShareCount,
+  RedditShareCount,
+  TumblrShareCount,
+  HatenaShareCount,
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  LinkedinShareButton,
+  TwitterShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  RedditShareButton,
+  EmailShareButton,
+  TumblrShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  ViberShareButton,
+  WorkplaceShareButton,
+  LineShareButton,
+  WeiboShareButton,
+  PocketShareButton,
+  InstapaperShareButton,
+  HatenaShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  VKIcon,
+  OKIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  RedditIcon,
+  TumblrIcon,
+  MailruIcon,
+  EmailIcon,
+  LivejournalIcon,
+  ViberIcon,
+  WorkplaceIcon,
+  LineIcon,
+  PocketIcon,
+  InstapaperIcon,
+  WeiboIcon,
+  HatenaIcon,
+} from '../src';
+
+import './Demo.css';
+import exampleImage from './react-share-pin-example.png';
+
+class Demo extends Component {
+  render() {
+
+    const shareUrl = window.location.href;
+    const title = "Compatilhamento" ;
+
+    return (
+      <div className="Demo__container">
+        <div className="Demo__some-network">
+          <FacebookShareButton
+            url={shareUrl}
+            quote={title}
+            className="Demo__some-network__share-button"
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+
+          <div>
+            <FacebookShareCount url={shareUrl} className="Demo__some-network__share-count">
+              {count => count}
+            </FacebookShareCount>
+          </div>
+        </div>
+
+        <div className="Demo__some-network">
+          <FacebookMessengerShareButton
+            url={shareUrl}
+            appId="521270401588372"
+            className="Demo__some-network__share-button"
+          >
+            <FacebookMessengerIcon size={32} round />
+          </FacebookMessengerShareButton>
+        </div>
+
+        <div className="Demo__some-network">
+          <TwitterShareButton
+            url={shareUrl}
+            title={title}
+            className="Demo__some-network__share-button"
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+
+          <div className="Demo__some-network__share-count">&nbsp;</div>
+        </div>
+
+        <div className="Demo__some-network">
+          <TelegramShareButton
+            url={shareUrl}
+            title={title}
+            className="Demo__some-network__share-button"
+          >
+            <TelegramIcon size={32} round />
+          </TelegramShareButton>
+
+          <div className="Demo__some-network__share-count">&nbsp;</div>
+        </div>
+
+        <div className="Demo__some-network">
+          <WhatsappShareButton
+            url={shareUrl}
+            title={title}
+            separator=":: "
+            className="Demo__some-network__share-button"
+          >
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+
+          <div className="Demo__some-network__share-count">&nbsp;</div>
+        </div>
+
+        <div className="Demo__some-network">
+          <LinkedinShareButton url={shareUrl} className="Demo__some-network__share-button">
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Demo;
